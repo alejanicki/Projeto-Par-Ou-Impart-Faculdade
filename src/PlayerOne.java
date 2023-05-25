ed26ec68e8bd60e8c18b874a8035a92b5edf7998
alejanicki <alessandrojanicki@hotmail.com>
2023-05-25 00:49:46 -0300

    feat: Implementando par ou impar

--- src/PlayerOne.java
+++ src/PlayerOne.java
@@ -16,6 +16,49 @@ public class PlayerOne {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
 
+            //Lógica de comunicação c/ servidor para jogar
+            while (true) {
+                // Recebe mensagem inicial do servidor
+                String serverMessage = in.readLine();
+                System.out.println(serverMessage);
+                int playerNum = Integer.parseInt(consoleInput.readLine());
+                out.println(playerNum);
+
+                // Recebe segunda mensagem do servidor
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+                String playerChoice = consoleInput.readLine();
+                out.println(playerChoice);
+
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+                // Recebe total do servidor
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+
+                // Recebe resultado do servidor
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+
+                // Recebe jogar novamente do servidor
+                serverMessage = in.readLine();
+                String playAgain;
+
+                System.out.println(serverMessage);
+                playAgain = consoleInput.readLine();
+
+                out.println(playAgain);
+
+                if (playAgain == "n") {
+                    serverMessage = in.readLine();
+                    System.out.println(serverMessage);
+                    break;
+                }
+            }
+
             //Fechar Socket
             socket.close();
         } catch (IOException e) {
--- src/Server.java
+++ src/Server.java
@@ -4,6 +4,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Server {
     public static void main(String[] args) {
@@ -19,6 +22,54 @@ public class Server {
             //Reader e Writer Jogador 1
             BufferedReader PlayerOneIn = new BufferedReader(new InputStreamReader(PlayerOneSocket.getInputStream()));
             PrintWriter PlayerOneOut = new PrintWriter(PlayerOneSocket.getOutputStream(), true);
+
+            //Jogar Par ou Impar c/ máquina
+            while (true) {
+                PlayerOneOut.println("Jogador 1, escolha um numero de 0 a 5: ");
+                int PlayerOneNum = Integer.parseInt(PlayerOneIn.readLine());
+                System.out.println("Numero Jogador 1: " + PlayerOneNum);
+
+                PlayerOneOut.println("Jogador 1, par ou impar? (p/i): ");
+                String PlayerOneChoice = PlayerOneIn.readLine();
+                System.out.println("Escolha Jogador 1: " + PlayerOneChoice);
+
+                Random random = new Random();
+                int randomNumber = random.nextInt(6);
+
+                List<String> letters = new ArrayList<>();
+                letters.add("p");
+                letters.add("i");
+                random = new Random();
+                int randomIndex = random.nextInt(letters.size());
+                String randomLetter = letters.get(randomIndex);
+
+                int result = PlayerOneNum + randomNumber;
+                PlayerOneOut.println("Jogador 1: " + "Numero - "+ PlayerOneNum + "Escolha - " + PlayerOneChoice);
+                PlayerOneOut.println("Jogador 2: " + "Numero - "+ randomNumber + "Escolha - " + randomLetter);
+                PlayerOneOut.println("Total: " + PlayerOneNum + " + " + randomNumber + " = " + result);
+
+                boolean isResultEven = result % 2 == 0;
+
+                if (PlayerOneChoice.contentEquals(randomLetter)) {
+                    PlayerOneOut.println("Empatou");
+                    System.out.println("Empatou");
+                } else if ((PlayerOneChoice.equals("p") && isResultEven)
+                        || (PlayerOneChoice.equals("i") && !isResultEven)) {
+                    PlayerOneOut.println("Jogador 1 venceu!");
+                    System.out.println("Jogador 1 venceu!");
+                } else {
+                    PlayerOneOut.println("Jogador 2 venceu!");
+                    System.out.println("Jogador 2 venceu!");
+                }
+
+                PlayerOneOut.println("Deseja jogar novamente? (s/n): ");
+                String PlayerOneReset = PlayerOneIn.readLine();
+
+                if (PlayerOneReset.equals("n")) {
+                    PlayerOneOut.println("FIM");
+                    break;
+                }
+            }
 
             //Fechar sockets
             PlayerOneSocket.close();
